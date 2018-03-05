e4228cf472ee1c4682700a524ff08ac1cb74fa0c
Nicolas Gunther <nicolas.gunther@ekino.com>
2018-03-05 18:23:45 +0100

    Init folder

---
+++ src/main/java/com/ekino/trombihack/TrombihackApplication.java
@@ -0,0 +1,12 @@
+package com.ekino.trombihack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrombihackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrombihackApplication.class, args);
+	}
+}
