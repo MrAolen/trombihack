dcc55d1b4b21e49c96d1be41dd89eba0a903d6b1
Nicolas Gunther <nicolas.gunther@ekino.com>
2018-03-06 10:14:15 +0100

    Init bot

---
+++ src/main/java/com/ekino/trombihack/bot/SlackBot.java
@@ -0,0 +1,32 @@
+package com.ekino.trombihack.bot;
+
+import me.ramswaroop.jbot.core.slack.Bot;
+import me.ramswaroop.jbot.core.slack.Controller;
+import me.ramswaroop.jbot.core.slack.EventType;
+import me.ramswaroop.jbot.core.slack.models.Event;
+import me.ramswaroop.jbot.core.slack.models.Message;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
+
+@Component
+public class SlackBot extends Bot{
+
+    @Value("${slackBotToken}")
+    private String slackToken;
+
+    @Override
+    public String getSlackToken() {
+        return slackToken;
+    }
+
+    @Override
+    public Bot getSlackBot() {
+        return this;
+    }
+
+    @Controller(events = {EventType.DIRECT_MENTION, EventType.DIRECT_MESSAGE})
+    public void onReceiveDM(WebSocketSession session, Event event) {
+        reply(session, event, new Message("Hi, I am " + slackService.getCurrentUser().getName()));
+    }
+}
