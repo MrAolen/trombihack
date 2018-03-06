c1bdf6ccdc9dcdd5752a2896abecf5043daf51d8
Nicolas Gunther <nicolas.gunther@ekino.com>
2018-03-06 10:30:45 +0100

    Add config bot

--- src/main/java/com/ekino/trombihack/TrombihackApplication.java
+++ src/main/java/com/ekino/trombihack/TrombihackApplication.java
@@ -3,7 +3,7 @@ package com.ekino.trombihack;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"me.ramswaroop.jbot"})
 public class TrombihackApplication {
 
 	public static void main(String[] args) {
