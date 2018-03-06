2034dc30a70614ce8285e25ecfa897c28152df4a
Nicolas Gunther <nicolas.gunther@ekino.com>
2018-03-06 11:37:28 +0100

    Add scan package

--- src/main/java/com/ekino/trombihack/TrombihackApplication.java
+++ src/main/java/com/ekino/trombihack/TrombihackApplication.java
@@ -3,7 +3,7 @@ package com.ekino.trombihack;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"me.ramswaroop.jbot"})
+@SpringBootApplication(scanBasePackages = {"me.ramswaroop.jbot", "com.ekino.trombihack.bot"})
 public class TrombihackApplication {
 
 	public static void main(String[] args) {
