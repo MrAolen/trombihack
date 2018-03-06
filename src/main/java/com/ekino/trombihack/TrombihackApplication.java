3fb1df723304ad53c19bbb0287ba1e93c53ebea3
Nicolas Gunther <nicolas.gunther@ekino.com>
2018-03-06 14:50:50 +0100

    Remove bot and replace by slash command

--- src/main/java/com/ekino/trombihack/TrombihackApplication.java
+++ src/main/java/com/ekino/trombihack/TrombihackApplication.java
@@ -3,7 +3,10 @@ package com.ekino.trombihack;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"me.ramswaroop.jbot", "com.ekino.trombihack.bot"})
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@SpringBootApplication(scanBasePackages = {"me.ramswaroop.jbot", "com.ekino.trombihack"})
 public class TrombihackApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/ekino/trombihack/service/RuleService.java
@@ -0,0 +1,12 @@
+package com.ekino.trombihack.service;
+
+import me.ramswaroop.jbot.core.slack.models.Message;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RuleService {
+
+    public void proceedCommand(String command) {
+
+    }
+}
